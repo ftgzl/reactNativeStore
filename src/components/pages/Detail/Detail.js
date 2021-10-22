8eacf4247d84f74250ebee2d4ad1154661fccdde
fuatguzel <fuatguzel754@gmail.com>
2021-10-22 23:56:46 +0300

    Create Detail pages.

---
+++ src/components/pages/Detail/Detail.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { SafeAreaView, Text } from 'react-native';
+
+const Detail = () => {
+  return (
+    <SafeAreaView>
+      <Text>Detail</Text>
+    </SafeAreaView>
+  )
+}
+
+export default Detail;
